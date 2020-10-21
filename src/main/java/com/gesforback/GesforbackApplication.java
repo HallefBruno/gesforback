16099623d66fe87425437f41cd60967f7b93ce27
Sud <Sud@Sud-PC>
2020-10-21 18:57:04 -0300

    Comit inicial

---
+++ src/main/java/com/gesforback/GesforbackApplication.java
@@ -0,0 +1,13 @@
+package com.gesforback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GesforbackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GesforbackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gesforback/GesforbackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gesforback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GesforbackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
